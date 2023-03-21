4dc78a43398d39b68210ca76bf797594b6d91e60
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-03-21 15:38:49 +0700

    sementara jalan

--- routes/web.php
+++ routes/web.php
@@ -29,63 +29,42 @@ Route::get('login', [AuthController::class, 'showFormLogin'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout']);
 
-Route::group(['middleware' => ['auth', 'ceklevel:admin,kepsek,guru_pns,guru_ptt']], function() {
+Route::group(['middleware' => ['auth', 'ceklevel:admin,kepsek,guru_pns,guru_ptt']], function () {
 
     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
 
     Route::resource('admin', AdminController::class);
-    
-    Route::resource('kepala-sekolah', KepalaSekolahController::class);
-    
+
     Route::resource('guru-pns', GuruPNSController::class);
-    
-    Route::resource('guru-ptt', GuruPTTController::class);
-
-
-
 });
 
-Route::group(['middleware' => ['auth', 'ceklevel:admin,kepsek']], function() {
+Route::group(['middleware' => ['auth', 'ceklevel:admin']], function () {
 
     Route::resource('admin', AdminController::class);
-    
+
     Route::resource('kepala-sekolah', KepalaSekolahController::class);
-    
+
     Route::resource('guru-pns', GuruPNSController::class);
-    
-    Route::resource('guru-ptt', GuruPTTController::class);
 
     Route::get('/laporan-absensi-pns', [LaporanAbsenController::class, 'laporanPNS']);
     Route::get('/filter-pns/{tglawal}/{tglakhir}', [LaporanAbsenController::class, 'filterPNS']);
     Route::get('/cetak-pns/{data1}/{data2}', [LaporanAbsenController::class, 'cetakPNS']);
 
-    Route::get('laporan-absensi-ptt', [LaporanAbsenController::class, 'laporanPTT']);
-    Route::get('/filter-ptt/{tglawal}/{tglakhir}', [LaporanAbsenController::class, 'filterPTT']);
-    Route::get('/cetak-ptt/{data1}/{data2}', [LaporanAbsenController::class, 'cetakPTT']);
 
     Route::get('lokasi-sekolah', [KoordinatSekolahController::class, 'index']);
     Route::post('ubah-koordinat', [KoordinatSekolahController::class, 'update']);
-
 });
 
 
-Route::group(['middleware' => ['auth', 'ceklevel:guru_pns']], function() {
-    
+Route::group(['middleware' => ['auth', 'ceklevel:guru_pns']], function () {
+
     Route::resource('absen-guru-pns', GuruPNSAbsenController::class);
     Route::post('absen-guru-pns-keluar', [GuruPNSAbsenController::class, 'absenKeluar'])->name('absen-pns-keluar');
-
 });
 
-Route::group(['middleware' => ['auth', 'ceklevel:guru_ptt']], function() {
-    
-    Route::resource('absen-guru-ptt', GuruPTTAbsenController::class);
-    Route::post('absen-guru-ptt-keluar', [GuruPTTAbsenController::class, 'absenKeluar'])->name('absen-ptt-keluar');
 
-});
 
-Route::group(['middleware' => ['auth', 'ceklevel:guru_pns,guru_ptt']], function() {
-    
+Route::group(['middleware' => ['auth', 'ceklevel:guru_pns']], function () {
+
     Route::view('lokasi-anda', 'pages.lokasi.lokasi-anda');
-
 });
-
